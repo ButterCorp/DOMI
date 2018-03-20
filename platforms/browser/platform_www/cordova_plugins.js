24bb6976d49ba62dc6f600da93d07cf30957c416
Pierre-Yves DERET <pierreyves.deret@gmail.com>
2018-03-20 23:00:13 +0100

    Page urgences + plugin call

--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -29,6 +29,14 @@ module.exports = [
         "clobbers": [
             "window.ApiAIPromises"
         ]
+    },
+    {
+        "file": "plugins/cordova-plugin-call-number/www/CallNumber.js",
+        "id": "cordova-plugin-call-number.CallNumber",
+        "pluginId": "cordova-plugin-call-number",
+        "clobbers": [
+            "call"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -36,7 +44,8 @@ module.exports.metadata =
 {
     "cordova-plugin-speechrecognition": "1.1.2",
     "cordova-plugin-whitelist": "1.3.3",
-    "cordova-plugin-apiai": "1.7.4"
+    "cordova-plugin-apiai": "1.7.4",
+    "cordova-plugin-call-number": "1.0.1"
 }
 // BOTTOM OF METADATA
 });
